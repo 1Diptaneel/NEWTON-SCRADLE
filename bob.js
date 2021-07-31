e03366c3207d0b96a537ac566155c4faec458e39
1Diptaneel <83410205+1Diptaneel@users.noreply.github.com>
2021-07-31 11:38:22 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -1,37 +1,24 @@
-class bob
-{
-	constructor(x,y,r)
-	{
-		var options={
-			isStatic:false,
-			restitution:1,
-			friction:0,
-			density:0.8
-			
-			}
-		this.x=x;
-		this.y=y;
-		this.r=r
-		
-		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
-		World.add(world, this.body);
+class Bob {
+    constructor(x,y,radius) {
+        var options = {
+            isStatic: false,
+            restitution: 1,
+            friction:0,
+            density:0.8
+        }
+        
+        this.body = Bodies.circle(x,y,radius, options);
+        this.radius = radius
 
-	}
-	display()
-	{
-			
-			var paperpos=this.body.position;
-			push()
-			translate(paperpos.x, paperpos.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			fill(255,0,255)
-			//imageMode(CENTER);
-			//image(this.image, 0,0,this.r, this.r)
-			ellipse(0,0,this.r, this.r);
-			pop()
-			
-	}
+        World.add(world, this.body);
+    }
+    display(){
+        
+        ellipseMode(RADIUS);
 
-}
+        fill("magenta");
 
+        circle(this.body.position.x, this.body.position.y, this.radius);
+
+    }       
+};
