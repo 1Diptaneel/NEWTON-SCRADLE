1f82a8ef393658d84f6cc05fc41e36d608aae864
1Diptaneel <83410205+1Diptaneel@users.noreply.github.com>
2021-08-03 16:37:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,60 +4,63 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
+function preload()
+{
+	
+}
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(600, 500);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
+	var bobDiameter = 40;
+
 	//Create the Bodies Here.
-	roof = new Roof (425,300,250,20);
-	bobObject1 = new Bob (340,470,20)
-	bobObject2 = new Bob (380,470,20)
-	bobObject3 = new Bob (420,470,20)
-	bobObject4 = new Bob (460,470,20)
-	bobObject5 = new Bob (500,470,20);
-	rope1= new Rope(bobObject1.body,roof.body,-40*2, 0);
-	rope2= new Rope(bobObject2.body,roof.body,-20*2,0);
-	rope3= new Rope(bobObject3.body,roof.body,-0*2,0);
-	rope4= new Rope(bobObject4.body,roof.body,20*2,0);
-	rope5= new Rope(bobObject5.body,roof.body,40*2,0);
+	roof = new Roof(300, 100, 300, 20);
+
+	bob1 = new Bob(200, 400, 50);
+	bob2 = new Bob(250, 400, 50);
+	bob3 = new Bob(300, 400, 50);
+	bob4 = new Bob(350, 400, 50);
+	bob5 = new Bob(400, 400, 50);
+
+	rope1 = new Rope(bob1.body,roof.body,-bobDiameter*2.4,0);
+	rope2 = new Rope(bob2.body,roof.body,-bobDiameter*1.3,0);
+	rope3 = new Rope(bob3.body,roof.body,0,0); 
+ 	rope4 = new Rope(bob4.body,roof.body,bobDiameter*1.3,0); 
+	rope5 = new Rope(bob5.body,roof.body,bobDiameter*2.4,0);
+
 	Engine.run(engine);
   
 }
 
 
 function draw() {
-  rectMo=(CENTER);
+  rectMode(CENTER);
   background(255);
-  
-  roof.display()
+
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
   rope1.display();
   rope2.display();
   rope3.display();
   rope4.display();
   rope5.display();
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display();
   
+  roof.display();
+  
+  drawSprites();
+ 
 }
 
-function drawLine (constraint) {
-	bobBodyPosition = constraint.bodyA.position;    
-	roofBodyPosition = constraint.bodyB.position;
-	roofBodyOffset = constraint.pointB;
-	roofBodyX= roofBodyPositiion.x + roofBodyOffset.x;
-	roofBodyY= roofBodyPosition.y + roofBodyOffset.y; 
-	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX, roofBodyY);           
-}
-
-function keyPressed () {
-	if (keyCode === UP_ARROW) {
-		Matter.Body.applyForce(bobObject1.body, bobObject1.body.position, {x: -50, y:-45});
-	}
-}
+function keyPressed() { 
+	if (keyCode === UP_ARROW) 
+	{ Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45}); } 
+} 
